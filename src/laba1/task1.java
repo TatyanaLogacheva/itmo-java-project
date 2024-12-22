86b013f8d01a1a7ccd66de58bd3e7f2305b107ad
Tatyana <logacheva.t.d@yandex.ru>
2024-12-22 23:43:34 +0300

    laba1

---
+++ src/laba1/task1.java
@@ -0,0 +1,10 @@
+package laba1;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        System.out.println("Я \nхорошо \nзнаю \nJava.");
+            }
+}
+
---
+++ src/laba1/task3.java
@@ -0,0 +1,9 @@
+package laba1;
+
+public class task3 {
+    public static void main(String[] args) {
+        short number = 10500;
+        int result = (number / 10) / 10;
+        System.out.println(result);
+    }
+}
