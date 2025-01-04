386ebe9d2aab3df30569d2858f739724f730fa4d
Tatyana <logacheva.t.d@yandex.ru>
2025-01-05 00:31:17 +0300

    laba4

---
+++ src/laba4/ArrayInclude1or3.java
@@ -0,0 +1,18 @@
+package laba4;
+
+public class ArrayInclude1or3 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, -16, 20, 48};
+        int counter = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == 0 || arr[i] == 3) {
+                counter++;
+                System.out.println("Массив содержит 1 или 3");
+                break;
+            }
+        }
+        if (counter == 0) {
+            System.out.println("Массив не содержит 1 и 3");
+        }
+    }
+}
