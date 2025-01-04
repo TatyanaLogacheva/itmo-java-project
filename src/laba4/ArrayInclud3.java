cc509144ebfb5c4605ff02ac8b037af3237ee185
Tatyana <logacheva.t.d@yandex.ru>
2025-01-05 00:30:15 +0300

    laba4

--- src/laba4/ArrayInclud3.java
+++ src/laba4/ArrayInclud3.java
@@ -1,7 +1,23 @@
 package laba4;
 
+import java.util.Arrays;
+
 public class ArrayInclud3 {
-    public static void main (String[] args){
-        int[]array;
+    public static boolean check(int a, int b) {
+        if (a == 3 || b == 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 3};
+        if (array.length >= 2) {
+            System.out.println("array = " + Arrays.toString(array));
+            System.out.print(check(array[0], array[array.length - 1]));
+        } else {
+            System.out.println("Массив не отвечает условиям");
+        }
     }
 }
