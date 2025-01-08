a786c00459ba666aeb16cec5b091f9c9ddd747da
Tatyana <logacheva.t.d@yandex.ru>
2025-01-08 18:24:24 +0300

    laba4

--- src/laba4_part2/CheckSort.java
+++ src/laba4_part2/CheckSort.java
@@ -1,4 +1,20 @@
 package laba4_part2;
 
 public class CheckSort {
-}
+    public static void main (String [] args){
+        boolean result=true;
+        int [] arr = {-2, 1, 8, 12, 25};
+        for (int i =0; i< arr.length-2; ++i) {
+            if (arr[i] < arr[i + 1]) {
+                continue;
+            } else {
+                result = false;
+                System.out.println("Please, try again");
+                break;
+            }
+        }
+            if (result){
+                System.out.println("ОК");
+            }
+        }
+    }
