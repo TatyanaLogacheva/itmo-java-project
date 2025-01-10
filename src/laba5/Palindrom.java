185e6604ecf100f93628a22d0e85ae93b7a03171
Tatyana <logacheva.t.d@yandex.ru>
2025-01-10 15:00:28 +0300

    correct

--- src/laba5/Palindrom.java
+++ src/laba5/Palindrom.java
@@ -2,12 +2,10 @@ package laba5;
 
 public class Palindrom {
     public static boolean palindromCheck(StringBuilder word) {
-        String word2 = word.toString();
+        String word2 = word.toString().toLowerCase();
         word.reverse();
-        word2 = word2.toLowerCase();
-        String word1 = word.toString();
-        word1 = word1.toLowerCase();
-        if (word2.equals(word1.toLowerCase())) {
+        String word1 = word.toString().toLowerCase();
+        if (word2.equals(word1)) {
             return true;
         } else {
             return false;
